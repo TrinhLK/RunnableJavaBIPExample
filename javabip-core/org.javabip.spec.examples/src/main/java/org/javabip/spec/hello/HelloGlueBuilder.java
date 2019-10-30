5c6aac01e6d919163332511a8d3d39cada46dbfe
Trinh Le-Khanh <trinhlk@Trinhs-MacBook-Pro.local>
2019-10-30 16:15:23 +0100

    sender-receiver done, one2one and one2three is similar

--- javabip-core/org.javabip.spec.examples/src/main/java/org/javabip/spec/hello/HelloGlueBuilder.java
+++ javabip-core/org.javabip.spec.examples/src/main/java/org/javabip/spec/hello/HelloGlueBuilder.java
@@ -8,11 +8,18 @@ public class HelloGlueBuilder extends GlueBuilder{
 	@Override
 	public void configure() {
 		// TODO Auto-generated method stub
-		port(HelloSender.class, "s").accepts(HelloReceiver.class, "r");
-		port(HelloReceiver.class, "r").requires(HelloSender.class, "s");
+		//One2Three
+//		port(HelloSender.class, "s").accepts(HelloReceiver.class, "r");
+//		port(HelloReceiver.class, "r").requires(HelloSender.class, "s");
 		
+		//One2One
+		port(HelloSender.class, "s").accepts(HelloMiddle.class, "t");
+		port(HelloMiddle.class, "t").requires(HelloSender.class, "s");
+		port(HelloMiddle.class, "t").accepts(HelloReceiver.class, "r");
+		port(HelloReceiver.class, "r").requires(HelloMiddle.class, "t");
 		//One to One
-		data(HelloSender.class, "sendId").to(HelloReceiver.class, "sendId");
+		data(HelloSender.class, "sendId").to(HelloMiddle.class, "sendId");
+		data(HelloMiddle.class, "transferedData").to(HelloReceiver.class, "transferedData");
 	}
 
 }
--- javabip-core/org.javabip.spec.examples/src/main/java/org/javabip/spec/hello/HelloReceiver.java
+++ javabip-core/org.javabip.spec.examples/src/main/java/org/javabip/spec/hello/HelloReceiver.java
@@ -2,12 +2,10 @@ package org.javabip.spec.hello;
 
 import org.javabip.annotations.ComponentType;
 import org.javabip.annotations.Data;
-import org.javabip.annotations.Guard;
 import org.javabip.annotations.Port;
 import org.javabip.annotations.Ports;
 import org.javabip.annotations.Transition;
 import org.javabip.api.PortType;
-import org.javabip.api.DataOut.AccessType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,19 +14,20 @@ import org.slf4j.LoggerFactory;
 public class HelloReceiver {
 
 	Logger logger = LoggerFactory.getLogger(HelloReceiver.class);
-	protected int sendId;
-	protected int receiveId;
+	private int data;
+	private int receiveId;
 	
 	public HelloReceiver(int id) {
-		this.sendId = -1;
+		this.data = -1;
 		receiveId = id;
 	}
 	
 	@Transition(name = "r", source = "START", target = "END")
-	public void receiving(@Data(name = "sendId") Integer id) {
-		this.sendId = id;
+	public void receiving(@Data(name = "transferedData") Integer id) {
+		this.data = id;
+		System.out.println("--- " + id);
 		if (receiveId == 1 || receiveId == 3)
-			System.out.println("I'm " + receiveId + ", Hello World received from " + sendId);
+			System.out.println("I'm " + receiveId + ", Hello World received from " + data);
 	}
 	
 }
